6538b9cf6d54a4be5029b3850a088317ef349754
jO <joel.schellenberger@gmail.com>
2016-07-25 19:13:28 -0400

    new jailbreak! starting up a new status bar theme :)

--- packages.sh
+++ packages.sh
@@ -4,3 +4,4 @@ dpkg-deb -bZgzip projects/acai debs
 dpkg-deb -bZgzip projects/softy debs
 dpkg-deb -bZgzip projects/starwars debs
 dpkg-deb -bZgzip projects/pablo debs
+dpkg-deb -bZgzip projects/tumble debs
