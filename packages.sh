8e8940c859757c0909e6c2d3fa9475adc73dd710
jO <joel.schellenberger@gmail.com>
2016-08-03 02:24:17 -0400

    remade some stuff, working on some other stuff!

--- packages.sh
+++ packages.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/pluto debs
 dpkg-deb -bZgzip projects/acai debs
-dpkg-deb -bZgzip projects/softy debs
-dpkg-deb -bZgzip projects/starwars debs
-dpkg-deb -bZgzip projects/pablo debs
 dpkg-deb -bZgzip projects/tumble debs
