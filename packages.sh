b404ba34ec7f7ed310ad2e5d94f08be8c8379fc5
jO <joel.schellenberger@gmail.com>
2016-08-05 00:21:20 -0400

    hosting smartlock

--- packages.sh
+++ packages.sh
@@ -3,3 +3,4 @@ dpkg-deb -bZgzip projects/pluto debs
 dpkg-deb -bZgzip projects/acai debs
 dpkg-deb -bZgzip projects/tumble debs
 dpkg-deb -bZgzip projects/elCapLoader debs
+dpkg-deb -bZgzip projects/smartLock debs
