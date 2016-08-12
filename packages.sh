8d5bfd2b9b6cb0c49a5f770d85378a42bda7f2cb
jO <joel.schellenberger@gmail.com>
2016-08-11 22:08:37 -0400

    bug fixing and depiction work

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/pluto debs
 dpkg-deb -bZgzip projects/acai debs
-dpkg-deb -bZgzip projects/tumble debs
-dpkg-deb -bZgzip projects/elCapLoader debs
+dpkg-deb -bZgzip projects/eMinor debs
+dpkg-deb -bZgzip projects/rainbowRoad debs
 dpkg-deb -bZgzip projects/smartLock debs
 dpkg-deb -bZgzip projects/repoSetup debs
